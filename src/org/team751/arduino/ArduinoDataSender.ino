cae2b81fbceba54b2c3172b10a29d24d6324656a
kennethakers <32209138+kennethakers@users.noreply.github.com>
2018-01-16 17:26:45 -0800

    Working with Serial protocol
    Still need to implement rotation, velocity, and distance traveled using Aidan's BNO055.ino.

--- src/org/team751/arduino/ArduinoDataSender.ino
+++ src/org/team751/arduino/ArduinoDataSender.ino
@@ -1,43 +1,49 @@
-#include <MillisTimer.h>
+/**
+   Author: Kenny Akers
+   Organization: Team 751 Barn2 Robotics
+   Competition: 2018 FRC
+   Email: kakers19@priorypanther.com
 
-unsigned long requestID;
-char requestToken = '-'; // Character used to separate command from ID number.
+   This Arduino program establishes a Serial communication protocol with the RoboRIO.
+   Currently, it is programmed to work with queries of the format:
+                  <query_token><separator_token><request_ID>
+                  Ex. Q-123
 
-MillisTimer timer = MillisTimer(10);
+   And to respond with the following format:
+                  <request_ID><separator><rotation_data><separator><distance_traveled><separator><velocity_data>
+                  Ex. 123-130-42-80
+
+   The Arduino will send this data 3 times unless it receives the confirmation token from the RoboRIO, at which
+   point it will stop. If it does not receive this token before it has sent the data 3 times, it will stop.
+*/
+
+unsigned long requestID; // Variable to hold the query ID of the incoming request.
+String separator = "-"; // Character used to separate command from ID number.
+String queryToken = "Q"; // Token used by the RoboRIO to initiate a request.
+String confirmationToken = "OK"; // Token used by the RoboRIO signal that it successfully received the correct response to the query (i.e. both request and response IDs match).
+int sendRepeat = 10; // Variable that denotes how many times the Arduino will send the queried information if it does not receive the confirmation token.
 
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(10);
-  timer.setInterval(1000);
-  timer.expiredHandler(myTimerFunction);
-  timer.setRepeats(3); // Resend 3 times.
-  timer.start();
 }
 
 void loop() {
   if (Serial.available() > 0) { // If the RoboRIO has sent a query command...
     String request = Serial.readString();
-    Serial.println(request);
-    if (request.charAt(0) == 'Q' && request.charAt(1) == '-') {
-      request = request.substring(request.indexOf("-") + 1);
+    //Serial.println(request);
+    if (request.startsWith(queryToken + separator)) {
+      request = request.substring(request.indexOf(separator) + 1);
       requestID = request.toInt();
-      Serial.println(requestID);
-      for (int i = 0; i < 3; i++) { // Want to send the data 3 times if RoboRIO doesn't send back "OK-###"
-        sendData();
-      }
+      //Serial.println(requestID);
+      sendData();
     }
   }
 }
 
-void myTimerFunction(MillisTimer &mt)
-{
-  Serial.print(Constant("Repeat: "));
-  Serial.println(mt.getRemainingRepeats());
-  timer.run();
-}
-
-void sendData() { <<IN PROGRESS>>
-  while (!Serial.readString().equals("OK-" + requestID)) { // While the RoboRIO hasn't sent the right verification token. 
+void sendData() {
+  int count = 0; // Counter so the sensor data is only sent a maximum of 3 times.
+  while (count++ < sendRepeat && !Serial.readString().equals(confirmationToken + separator + String(requestID))) { // While the RoboRIO hasn't sent the right verification token.
     Serial.print(requestID);
     Serial.print("-");
     Serial.print(getRotation());
@@ -50,13 +56,13 @@ void sendData() { <<IN PROGRESS>>
 }
 
 int getRotation() {
-
+  return 420;
 }
 
 int getDistanceTraveled() {
-
+  return 69;
 }
 
 int getVelocity() {
-
+  return 360;
 }
